a9875605b8f4f38281c5e320c2b93bf952463fa9
Maaya <maayajichuan@gmail.com>
2026-03-10 21:44:41 +0900

    Update bracket-function.js
    added a function for checking validity of the placement of brackets and a function for calling solve function for input inside brackets

--- bracket-function.js
+++ bracket-function.js
@@ -1,5 +1,6 @@
 let input = ["1", "+", "(", "2", "+", "3", ")"];
-
+let stack = [];
+let paraCount = 0;
 function isCorrect(){
   //if the bracket button was pressed but the previously pushed element was a digit, return false
   if(element === "(" && !operators.includes(display[display.length-1])){
@@ -7,6 +8,38 @@ function isCorrect(){
 }else{
     return true;
 }
+}
+//function for checking whether brackets are properly placed. 
+//only checks whether brackets are in pairs and not ")(" like a broken marriage
+function bracketsValid(nums){
+  nums.forEach((ele) => {
+    if(ele === "("){
+      paraCount++;
+    }else if(ele === ")"){
+      if(paraCount % 2 === 1){
+        paraCount = 0;
+      }else{
+        return false;
+      }
+    }
+    return paraCount = 0;
+  })
+}
+
+
+function bracketSolve(token){
+  token.forEach((ele, index) => {
+    let start;
+    if(ele ===  "("){
+      start = index;
+    }else if(ele === ")"){
+      let inner = token.slice(start+1, index);
+      token.splice(start, index-start+1); 
+      //call solve function
+      let result;
+      token.splice(start, 0, result);
+    }
+  })
 }
 
 let pushElement = function(element){//push numbers and operators into display array once buttons are clicked
